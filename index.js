0fbb9a4f21abb4ebb7da790e5628f6eb2c44882f
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-02-21 23:42:44 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 10;
+const num4 = 6;
+const multiply = multiplyem();
+const random = randomnum();
+const mod = remainder();
+const max = biggest();
 
-// Write your code here
+function multiplyem() {
+    return num1 * num2 
+}
+
+function randomnum() {
+   return Math.floor(Math.random() * 10) + 1;
+}
+
+function remainder() {
+    return num3 % num4;
+}
+
+function biggest() {
+    return Math.max(10, 20);
+}
+
+
